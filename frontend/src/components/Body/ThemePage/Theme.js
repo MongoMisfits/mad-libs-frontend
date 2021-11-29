436e8f44b562915f509064c4cd488e2de45ebc4c
Tom Fairchild <tomsingleton@Toms-iMac.local>
2021-11-29 11:37:29 -0800

    package-lock json installs and ThemePage updates

--- frontend/src/components/Body/ThemePage/Theme.js
+++ frontend/src/components/Body/ThemePage/Theme.js
@@ -1,6 +1,25 @@
 import React from 'react'
+import BacktotheFuture from '../../../themes/BacktotheFuture.jpeg'
+import Batman from '../../../themes/Batman.jpeg'
+import DragonBall from '../../../themes/DragonBall.jpeg'
+import FullmetalAlchemist from '../../../themes/FullmetalAlchemist.jpeg'
+import Ghostbusters from '../../../themes/Ghostbusters.jpeg'
+import Godzilla from '../../../themes/Godzilla.jpeg'
+import JurassicPark from '../../../themes/JurassicPark.jpeg'
+import Lordoftherings from '../../../themes/Lordoftherings.jpeg'
+import Marvel from '../../../themes/Marvel.jpeg'
+import Matrix from '../../../themes/Matrix.jpeg'
+import RickandMorty from '../../../themes/RickandMorty.jpeg'
+import SpiderMan from '../../../themes/SpiderMan.jpeg'
+import StarWars from '../../../themes/StarWars.jpeg'
+import Superman from '../../../themes/Superman.jpeg'
+import Supernatural from '../../../themes/Supernatural.jpeg'
+import XMen from '../../../themes/XMen.jpeg'
 
 function Theme() {
+
+    
+
     return (
         <div>
             Themes Section
