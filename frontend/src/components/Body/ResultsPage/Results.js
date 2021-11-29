a7966b6db7ac33a0c5327996efde1d908cf5ba9a
brian99na <brian99na@gmail.com>
2021-11-29 00:01:41 -0800

    finishing up other user data on results page

--- frontend/src/components/Body/ResultsPage/Results.js
+++ frontend/src/components/Body/ResultsPage/Results.js
@@ -1,38 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Results(props) {
-    const [fullSentence, setFullSentence] = useState('')
+  const [fullSentence, setFullSentence] = useState([]);
+  const [otherUserData, setOtherUserData] = useState([]);
+  const [matchingUsers, setMatchingUsers] = useState([])
 
-    const concatFunction = () => {
-        if (props.gameBodyResults.blanks) {
-            let blankArr = props.gameBodyResults.blanks
-            let sentenceArr = props.gameBodyResults.value
-            let sentenceConcat = []
-            for (let i = 0; i < blankArr.length; i++) {
-                sentenceConcat.push(<span>{sentenceArr[i]}</span>)
-                sentenceConcat.push(<span className='underlinedWords'>{blankArr[i]}</span>)
-            }
-            if (blankArr.length < sentenceArr.length) {
-                sentenceConcat.push(<span>{sentenceArr[sentenceArr.length - 1]}</span>)
-            }
-            setFullSentence(sentenceConcat)
-        }
+  const concatFunction = (blanks, sentences) => {
+    if (blanks) {
+      let blankArr = blanks;
+      let sentenceArr = sentences;
+      let sentenceConcat = [];
+      for (let i = 0; i < blankArr.length; i++) {
+        sentenceConcat.push(<span>{sentenceArr[i]}</span>);
+        sentenceConcat.push(
+          <span className="underlinedWords">{blankArr[i]}</span>
+        );
+      }
+      if (blankArr.length < sentenceArr.length) {
+        sentenceConcat.push(<span>{sentenceArr[sentenceArr.length - 1]}</span>);
+      }
+      setFullSentence([...fullSentence, sentenceConcat]);
     }
+  };
 
-    console.log(props.gameBodyResults)
+  const fetchData = () => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((users) => setOtherUserData(users));
+  };
 
-    useEffect(() => {
-        concatFunction()
-    }, [props.gameBodyResults])
+  const checkTemplate = () => {
+    const templateData = otherUserData.filter(user => user.template[0].title === props.gameBodyResults.title)
+    setMatchingUsers(templateData)
+  }
 
-    return (
-        <div className='resultsMain'>
-            {props.gameBodyResults && <h1>{props.gameBodyResults.title}</h1>}
-            <div className='resultsBody'>
-                <h1>{fullSentence}</h1>
-            </div>
-        </div>
-    )
+  console.log(matchingUsers)
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    concatFunction(props.gameBodyResults.blanks, props.gameBodyResults.value);
+    checkTemplate()
+  }, [props.gameBodyResults]);
+
+  const matchingUserJsx = matchingUsers.map((user) => {
+      return(
+          <div>
+              <h1>Username: {user.user}</h1>
+
+          </div>  
+      )
+  })
+
+  return (
+    <div className="resultsMain">
+      {props.gameBodyResults && <h1>{props.gameBodyResults.title}</h1>}
+      <div className="resultsBody">
+        <h1>{fullSentence}</h1>
+      </div>
+      <h1>View more from this category</h1>
+    </div>
+  );
 }
 
-export default Results
+export default Results;
