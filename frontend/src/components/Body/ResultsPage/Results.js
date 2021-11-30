e5def540b0d24a6633b030fd5f8fb65b77168dac
brian99na <brian99na@gmail.com>
2021-11-29 23:43:57 -0800

    finished styling on results page

--- frontend/src/components/Body/ResultsPage/Results.js
+++ frontend/src/components/Body/ResultsPage/Results.js
@@ -9,11 +9,18 @@ function Results(props) {
   const [usersDataSentence, setUsersDataSentence] = useState([])
   const [userStory, setUserStory] = useState([])
   const [usernameValue, setUsernameValue] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault()
     setUserStory([{user: usernameValue, template: [props.gameBodyResults]}])
     setUsernameValue('')
+    if (usernameValue.length > 3) {
+      saveData()
+      setErrorMessage('')
+    } else {
+      setErrorMessage(<h1 className='errorMessage'>Your username is too short. Please try again.</h1>)
+    }
   }
 
   const handleChange = (e) => {
@@ -22,25 +29,51 @@ function Results(props) {
 
   const concatFunction = (blanks, sentences) => {
     if (blanks) {
-      let blankArr = blanks;
-      let sentenceArr = sentences;
       let sentenceConcat = [];
-      for (let i = 0; i < blankArr.length; i++) {
-        sentenceConcat.push(<span>{sentenceArr[i]}</span>);
+      for (let i = 0; i < blanks.length; i++) {
+        sentenceConcat.push(<span>{sentences[i]}</span>);
         sentenceConcat.push(
-          <span className="underlinedWords">{blankArr[i]}</span>
+          <span className="underlinedWords">{blanks[i]}</span>
         );
       }
-      if (blankArr.length < sentenceArr.length) {
-        sentenceConcat.push(<span>{sentenceArr[sentenceArr.length - 1]}</span>);
+      if (blanks.length < sentences.length) {
+        sentenceConcat.push(<span>{sentences[sentences.length - 1]}</span>);
       }
-      if (fullSentence.length >= 1) {
-        setUsersDataSentence([...usersDataSentence, sentenceConcat])
-      } else {
-        setFullSentence([...fullSentence, sentenceConcat]);
-      }
+      setFullSentence([sentenceConcat]);
     }
   };
+
+
+  const concatFunctionUsers = (blanks, sentences) => {
+    if (blanks) {
+      let sentenceConcat = [];
+      for (let i = 0; i < blanks.length; i++) {
+        sentenceConcat.push(<span>{sentences[i]}</span>);
+        sentenceConcat.push(
+          <span className="underlinedWords">{blanks[i]}</span>
+        );
+      }
+      if (blanks.length < sentences.length) {
+        sentenceConcat.push(<span>{sentences[sentences.length - 1]}</span>);
+      }
+      setUsersDataSentence([...usersDataSentence, sentenceConcat])
+    }
+  };
+
+  const createUserSentences = () => {
+      for (let i=0; i < usersData.length; i++) {
+        let index = usersData[i].template[0]
+        let blanks = index.blanks
+        let sentences = index.value
+        concatFunctionUsers(blanks, sentences)
+      } 
+  }
+
+  const otherUserFetch = () => {
+    fetch('https://mongo-misfits.herokuapp.com/users')
+    .then(res => res.json())
+    .then(data => setUsersData(data))
+  }
 
   const saveData = () => {
     fetch("https://mongo-misfits.herokuapp.com/users", {
@@ -50,34 +83,30 @@ function Results(props) {
       },
       body: JSON.stringify(userStory)
     })
-      .then((res) => res.json())
   };
-
-  const otherUserFetch = () => {
-    fetch('https://mongo-misfits.herokuapp.com/users')
-    .then(res => res.json())
-    .then(data => setUsersData(data))
-  }
 
   useEffect(() => {
     otherUserFetch()
   }, [])
 
   useEffect(() => {
-    if (userStory) {
-      saveData()
-    }
-  }, [userStory])
-
-  useEffect(() => {
-    if (usersData[0] && usersData[0].template && usersData[0].template[0]) {
-      for (let i=0; i < usersData.length; i++) {
-        concatFunction(usersData[i].template[0].blanks, usersData[i].template[0].value)
-      } 
-    }
+    usersData && createUserSentences()
   }, [usersData])
 
-  console.log(props.gameBodyResults)
+  console.log(usersData)
+  console.log(usersDataSentence)
+
+
+  const otherUsersArr = usersData.map((user, index) => {
+    return(
+      <div className='otherUsers'>
+        <h1>Username: {user.user}</h1>
+        <h2>Story: {user.template[0].title}</h2>
+        <p>{usersDataSentence[index]}</p>
+      </div>
+
+    )
+  })
 
   useEffect(() => {
     concatFunction(props.gameBodyResults.blanks, props.gameBodyResults.value);
@@ -93,9 +122,13 @@ function Results(props) {
         <input onChange={handleChange} value={usernameValue}placeholder='Enter a name to save your story'/>
         <button type='submit'>Save Your Story</button>
       </form>
+      {errorMessage}
       <div className='resultsBtnArr'>
         <Link to='/game-page' > <button>Play Again</button></Link>
         <Link to='/thank-you-page' > <button>Done</button></Link>
+      </div>
+      <div className='otherUsersArrMain'>
+        {otherUsersArr}
       </div>
     </div>
   );
