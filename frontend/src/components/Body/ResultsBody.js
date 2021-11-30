e4fcbd7e13faa70419d3fc0a34f3f51cb2bab90a
brian99na <brian99na@gmail.com>
2021-11-30 09:55:16 -0800

    removed bg image for results page

--- frontend/src/components/Body/ResultsBody.js
+++ frontend/src/components/Body/ResultsBody.js
@@ -19,104 +19,104 @@ import XMen from "../../image/XMen.jpeg";
 import "./Body.css";
 
 function ResultsBody(props) {
-  if (document.querySelector(".result-page")) {
-    if (props.gameBodyResults.title === "Star Wars") {
-      const imageURL = StarWars;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Dragon Ball") {
-      const imageURL = DragonBall;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Marvel") {
-      const imageURL = Marvel;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Rick and Morty") {
-      const imageURL = RickandMorty;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "The Lord of the Rings") {
-      const imageURL = LordoftheRings;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Superman") {
-      const imageURL = Superman;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Batman") {
-      const imageURL = Batman;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "X-Men") {
-      const imageURL = XMen;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Spider-Man") {
-      const imageURL = SpiderMan;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Godzilla") {
-      const imageURL = Godzilla;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "The Matrix") {
-      const imageURL = Matrix;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Supernatural") {
-      const imageURL = Supernatural;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Jurassic Park") {
-      const imageURL = JurassicPark;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Back to the Future") {
-      const imageURL = BacktotheFuture;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Fullmetal Alchemist") {
-      const imageURL = FullmetalAlchemist;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-    if (props.gameBodyResults.title === "Ghostbusters") {
-      const imageURL = Ghostbusters;
-      document.querySelector(
-        ".result-page"
-      ).style.backgroundImage = `url('${imageURL}')`;
-    }
-  }
+  // if (document.querySelector(".result-page")) {
+  //   if (props.gameBodyResults.title === "Star Wars") {
+  //     const imageURL = StarWars;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Dragon Ball") {
+  //     const imageURL = DragonBall;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Marvel") {
+  //     const imageURL = Marvel;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Rick and Morty") {
+  //     const imageURL = RickandMorty;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "The Lord of the Rings") {
+  //     const imageURL = LordoftheRings;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Superman") {
+  //     const imageURL = Superman;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Batman") {
+  //     const imageURL = Batman;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "X-Men") {
+  //     const imageURL = XMen;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Spider-Man") {
+  //     const imageURL = SpiderMan;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Godzilla") {
+  //     const imageURL = Godzilla;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "The Matrix") {
+  //     const imageURL = Matrix;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Supernatural") {
+  //     const imageURL = Supernatural;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Jurassic Park") {
+  //     const imageURL = JurassicPark;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Back to the Future") {
+  //     const imageURL = BacktotheFuture;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Fullmetal Alchemist") {
+  //     const imageURL = FullmetalAlchemist;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  //   if (props.gameBodyResults.title === "Ghostbusters") {
+  //     const imageURL = Ghostbusters;
+  //     document.querySelector(
+  //       ".result-page"
+  //     ).style.backgroundImage = `url('${imageURL}')`;
+  //   }
+  // }
 
   return (
     <div className="result-page">
